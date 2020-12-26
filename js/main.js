9c6d5a8548600d1925da1a4d6ffacd0cd5f787e7
Magic4Me <65305684+magic4me@users.noreply.github.com>
2020-12-26 15:46:57 +0100

    website

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function googleTranslateElementInit() {
+        new google.translate.TranslateElement(
+            {pageLanguage: 'uk'
+            layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL
+            },
+            'google_translate_element'
+        );
+}
