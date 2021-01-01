559b3f5cb76dcf156e6bfe803aecf90b77865ea6
Magic4Me <65305684+magic4me@users.noreply.github.com>
2021-01-01 16:31:08 +0100

    website

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+(async()=>{if("loading"in HTMLImageElement.prototype){const a=document.querySelectorAll("img.lazyload");a.forEach(a=>{a.src=a.dataset.src})}else{await import("https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js");lazySizes.init()}})();
 function googleTranslateElementInit() {
         new google.translate.TranslateElement(
             {pageLanguage: 'uk',
